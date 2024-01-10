2a8be32b8a0baa285258d908207bd4c21b035bf1
Shivam <Shivamlather5@gmail.com>
2024-01-10 17:03:35 +0530

    Added logic to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,36 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const {Validation} = require('./types')
+const {User} = require('./db')
 
-app.use(expres.json())
+app.use(express.json())
 app.use(cors())
 
 app.post("/card",async (req,res)=>{
+    const response  = Validation.safeParse(req.body)
+    if(!response.success){
+        res.status(400).json({
+            msg:"Invalid inputs"
+        })
+    }
+    await User.create({
+        name:req.body.name,
+        description:req.body.description,
+        interest:req.body.interest,
+        facebook:req.body.facebook,
+        linkedin:req.body.linkedin
 
+    })
+    res.status(200).json({
+        msg:"User added successfully"
+    })
 })
 
 app.get("/cards", async (req,res)=>{
+    const response = await User.find({})
+    res.json({
+        cards: response
+    })
     
 })    
