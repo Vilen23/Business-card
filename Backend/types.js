96e25cb77580917652cc8874cf00c2cc6a95ce77
Shivam <Shivamlather5@gmail.com>
2024-01-11 03:25:51 +0530

    Added restrictions to zod

--- Backend/types.js
+++ Backend/types.js
@@ -1,11 +1,11 @@
 const zod = require('zod')
 
 const Validation = zod.object({
-    name: zod.string(),
-    description: zod.string(),
-    interest: zod.string(),
-    facebook: zod.string(),
-    linkedin: zod.string()
+    name: zod.string().min(1),
+    description: zod.string().min(5),
+    interest: zod.string().min(5),
+    facebook: zod.string().min(5),
+    linkedin: zod.string().min(5)
 
 })
 
