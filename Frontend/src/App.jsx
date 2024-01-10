a31f94d5f53ea22f5939fa3774dc67c5a296650a
Shivam <Shivamlather5@gmail.com>
2024-01-10 18:12:47 +0530

    Added logic to show card fromdb

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { CreateCard } from './Components/CreateCard'
+import { Card } from './Components/Card'
 
 function App() {
+const [cards,setCards] = useState([])
+fetch("http://localhost:3000/cards")
+  .then(async (resolve)=>{
+    const json = await resolve.json()
+    setCards(json.cards)
+  })
+
 
   return (
     <div>
       <CreateCard></CreateCard>
+      <Card cards={cards}></Card>
     </div>
   )
 }
