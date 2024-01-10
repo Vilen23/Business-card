45e631b417d4b710573a7f38b7b160736c113573
Shivam <Shivamlather5@gmail.com>
2024-01-10 16:16:39 +0530

    Basic boiler

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(expres.json())
+app.use(cors())
+
+app.post("/card",async (req,res)=>{
+
+})
+
+app.get("/cards", async (req,res)=>{
+    
+})    
